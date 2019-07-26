3aba96f4bcf1dfe1452927c860bdc4d0a7c9b7da
John Azzaro <jazzaro@temple.edu>
2019-07-25 22:20:49 -0700

    pug and static demo, added view and public folders

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ const nodemailer = require('nodemailer');                                   /* l
 const app = express();                                                      /* initialize app by calling express and storing as "app" */
     
                                                                         /* Fourth, setup your view engine to view static templates.  This will set your app views folder to something like "/users/project/myApp/views" */
-app.set('views', path.join(__dirname, 'views'));                           /* This tells pug where the template files will be. and we pass 'views' as the first param and second param use path.join (normalize al args into a path string) is the location (directory name is "views") */       
-app.set('view engine', 'pug')                                              /* this tells express to use the pug view "templating" engine*/
+// app.set('views', path.join(__dirname, 'views'));                           /* This tells pug where the template files will be. and we pass 'views' as the first param and second param use path.join (normalize al args into a path string) is the location (directory name is "views") */       
+// app.set('view engine', 'pug')                                           /* IF you would like to use the pug templating engine, set this and in app.get, use 'index'*/
+
+app.use(express.static(__dirname + '/public'));                            /* IF you would like to use static HTML files, you only need to set this and in app.get, use index.html*/
 
                                                                         /* Third, set up your middleware with app.use.  app.use applies the specified middleware to the main app stack in the order they are listed A->B->C */
 app.use(bodyParser.json())                                                  /* bodyparser middleware that can parse json*/
 app.use(bodyParser.urlencoded({extended: false}));                          /* bodyparser middleware that can parse url */
 
 app.get('/', function(req, res) {                                       /* GET request that sends back a response: */
-    res.render('index');
+    res.render('index.html');
 });
 
 app.listen(3000, function() {                                           /* listen on port 3000 */
